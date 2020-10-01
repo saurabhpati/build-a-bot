5dda2bd8a6e36802d401c86ca038c749800df8c4
Saurabh Pati <saurabhpati.pati@gmail.com>
2020-10-01 22:14:47 +0530

    builder.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'linebreak-style': 0
+    'linebreak-style': 0,
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
   },
 };
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,43 +1,206 @@
 <template>
-      <div>
+  <div>
     <div class="top-row">
       <div class="top part">
-        <img src="./images/head-big-eye.png" title="head"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+        <img v-bind:src="availableParts.heads[selectedHeadIndex].src" title="head" />
+        <button v-on:click="selectPreviousHead" class="prev-selector">&#9668;</button>
+        <button v-on:click="selectNextHead" class="next-selector">&#9658;</button>
       </div>
     </div>
     <div class="middle-row">
       <div class="left part">
-        <img src="./images/arm-articulated-claw.png" title="left arm"/>
-        <button class="prev-selector">&#9650;</button>
-        <button class="next-selector">&#9660;</button>
+        <img v-bind:src="availableParts.arms[selectedLeftArmIndex].src" title="left arm" />
+        <button v-on:click="selectPreviousLeftArm" class="prev-selector">&#9650;</button>
+        <button v-on:click="selectNextLeftArm" class="next-selector">&#9660;</button>
       </div>
       <div class="center part">
-        <img src="./images/torso-flexible-gauged.png" title="left arm"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+        <img v-bind:src="availableParts.torsos[selectedTorsoIndex].src" title="left arm" />
+        <button v-on:click="selectPreviousTorso" class="prev-selector">&#9668;</button>
+        <button v-on:click="selectNextTorso" class="next-selector">&#9658;</button>
       </div>
       <div class="right part">
-        <img src="./images/arm-dual-claw.png" title="left arm"/>
-        <button class="prev-selector">&#9650;</button>
-        <button class="next-selector">&#9660;</button>
+        <img v-bind:src="availableParts.arms[selectedRightArmIndex].src" title="left arm" />
+        <button v-on:click="selectPreviousRightArm" class="prev-selector">&#9650;</button>
+        <button v-on:click="selectNextRightArm" class="next-selector">&#9660;</button>
       </div>
     </div>
     <div class="bottom-row">
       <div class="bottom part">
-        <img src="./images/base-single-wheel.png" title="left arm"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+        <img v-bind:src="availableParts.bases[selectedBaseIndex].src" title="left arm" />
+        <button v-on:click="selectPreviousBase" class="prev-selector">&#9668;</button>
+        <button v-on:click="selectNextBase" class="next-selector">&#9658;</button>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
-export default {
-    name: 'RobotBuilder'
+/* eslint-disable */
+import availableParts from '../data/parts';
+
+function getPreviousComponentIndex(index, length) {
+  return index <= 0 ? length - 1 : index - 1;
 }
+
+function getNextComponentIndex(index, length) {
+  return index >= length - 1 ? 0 : index + 1;
+}
+
+export default {
+  name: 'RobotBuilder',
+  data() {
+    return {
+      availableParts,
+      selectedHeadIndex: 0,
+      selectedLeftArmIndex: 0,
+      selectedRightArmIndex: 0,
+      selectedTorsoIndex: 0,
+      selectedBaseIndex: 0,
+    };
+  },
+  methods: {
+    selectNextHead() {
+      this.selectedHeadIndex = getNextComponentIndex(
+        this.selectedHeadIndex,
+        availableParts.heads.length);
+    },
+    selectPreviousHead() {
+      this.selectedHeadIndex = getPreviousComponentIndex(
+        this.selectedHeadIndex,
+        availableParts.heads.length);
+    },
+    selectNextLeftArm() {
+      this.selectedLeftArmIndex =  getNextComponentIndex(
+        this.selectedLeftArmIndex,
+        availableParts.arms.length);
+    },
+    selectPreviousLeftArm() {
+      this.selectedLeftArmIndex =  getPreviousComponentIndex(
+        this.selectedLeftArmIndex,
+        availableParts.arms.length);
+    },
+    selectNextRightArm() {
+      this.selectedRightArmIndex =  getNextComponentIndex(
+        this.selectedRightArmIndex,
+        availableParts.arms.length);
+    },
+    selectPreviousRightArm() {
+      this.selectedRightArmIndex =  getPreviousComponentIndex(
+        this.selectedRightArmIndex,
+        availableParts.arms.length);
+    },
+    selectNextTorso() {
+      this.selectedTorsoIndex =  getNextComponentIndex(
+        this.selectedTorsoIndex,
+        availableParts.torsos.length);
+    },
+    selectPreviousTorso() {
+      this.selectedTorsoIndex =  getPreviousComponentIndex(
+        this.selectedTorsoIndex,
+        availableParts.torsos.length);
+    },
+    selectNextBase() {
+      this.selectedBaseIndex =  getNextComponentIndex(
+        this.selectedBaseIndex,
+        availableParts.bases.length);
+    },
+    selectPreviousBase() {
+      this.selectedBaseIndex =  getPreviousComponentIndex(
+        this.selectedBaseIndex,
+        availableParts.bases.length);
+    },
+  },
+};
 </script>
 
+<style scoped>
+.part {
+  position: relative;
+  width: 165px;
+  height: 165px;
+  border: 3px solid #aaa;
+}
+.part img {
+  width: 165px;
+}
+.top-row {
+  display: flex;
+  justify-content: space-around;
+}
+.middle-row {
+  display: flex;
+  justify-content: center;
+}
+.bottom-row {
+  display: flex;
+  justify-content: space-around;
+  border-top: none;
+}
+.head {
+  border-bottom: none;
+}
+.left {
+  border-right: none;
+}
+.right {
+  border-left: none;
+}
+.left img {
+  transform: rotate(-90deg);
+}
+.right img {
+  transform: rotate(90deg);
+}
+.bottom {
+  border-top: none;
+}
+.prev-selector {
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  left: -28px;
+  width: 25px;
+  height: 171px;
+}
+.next-selector {
+  position: absolute;
+  z-index: 1;
+  top: -3px;
+  right: -28px;
+  width: 25px;
+  height: 171px;
+}
+.center .prev-selector,
+.center .next-selector {
+  opacity: 0.8;
+}
+.left .prev-selector {
+  top: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
+}
+.left .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
+}
+.right .prev-selector {
+  top: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  right: -3px;
+}
+</style>
