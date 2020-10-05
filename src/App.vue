b2ef39fb716e38672f65bfd5482be3bf9c2cf30c
Saurabh Pati <saurabhpati.pati@gmail.com>
2020-10-05 21:23:58 +0530

    vue-router and nav routes.

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,28 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img src="./assets/build-a-bot-logo.png" class="logo"/> Build-a-bot
+            <router-link class="nav-link" :to="{ name: 'Home' }" exact>
+              <img src="./assets/build-a-bot-logo.png" class="logo"/> Build-a-bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'Build' }" exact>
+              Build
+            </router-link>
           </li>
         </ul>
       </nav>
     </header>
     <main>
-      <RobotBuilder/>
+      <router-view></router-view>
     </main>
   </div>
 </template>
 
 <script>
-// import HomePage from './home/HomePage.vue';
-import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
-  components: {
-    RobotBuilder,
-  },
 };
 </script>
 
@@ -63,5 +65,12 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
 }
 </style>
