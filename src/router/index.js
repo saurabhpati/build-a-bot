4f0a41881b8b2cc184ba6c13bb1fad81ce7a3151
Saurabh Pati <saurabhpati.pati@gmail.com>
2020-10-11 21:36:42 +0530

    history mode routing.

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import SidebarBuild from '../sidebar/SidebarBuild.vue';
 Vue.use(Router);
 
 export default new Router({
+    mode: 'history',
     routes: [{
         path: '/',
         name: 'Home',
